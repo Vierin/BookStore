c901399a1da1782b716afadeae9dbddef996b4b0
Ivan_Vierin <ivan@huncwot.com>
2024-02-14 19:03:16 +0100

    .

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,14 @@
+import  { FC } from 'react'
+import Header from '../../components/scaffold/header/Header'
+import Hero from '../../components/modules/hero/Hero'
+
+const Home:FC = () => {
+  return (
+    <>
+        <Header/>
+        <Hero/>
+    </>
+  )
+}
+
+export default Home
